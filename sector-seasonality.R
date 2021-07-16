b94c11d1ba92ea0b3eb51eb7ec0976bf56a23455
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2021-07-16 10:33:36 -0600

    Update sector-seasonality.R
    Added a backtest for 2021

--- sector-seasonality.R
+++ sector-seasonality.R
@@ -1,3 +1,5 @@
+#Analyze median sector returns by month of the year to find the best-performing sectors for each month
+
 library(tidyverse)
 library(tidyquant)
 options(digits=2)
@@ -44,3 +46,43 @@ ggsave(filename = "sectorseasonality.jpg",
        width = (1920*1.5)/300,
        height = (1080*1.5)/300,
        units = "in")
+
+#Backtest a sector-switching strategy for 2021 to compare YTD returns for sector-switching vs. holding the equal-weight index
+
+switching <- data.frame(month = 1:12,ticker = c("XBI","XAR","EWRE","RYE","XSD","EWRE","XSD","XSD","RTM","XTN","XTN","XME"))
+month_of_year = c("January","February","March","April","May","June","July","August","September","October","November","December")
+start <- as.Date("12-31-2020",format="%m-%d-%Y")
+end <- min(Sys.Date(),as.Date("12-31-2021",format="%m-%d-%Y"))
+
+RSP_data <- tq_get("RSP",from = start,to = end)
+RSP_monthly_return <- RSP_data %>% 
+  mutate(month = month(date),return = c(NA,diff(adjusted))/lag(adjusted)) %>%
+  group_by(month) %>% 
+  summarize(return = sum(return)) %>%
+  filter(!is.na(return)) %>%
+  mutate(month = as.numeric(month)) %>%
+  arrange(month) %>%
+  mutate(ticker = rep("RSP"),
+         strategy = "Index strategy")
+
+return_finder <- function(ticker){
+  price_data <- tq_get(ticker,from = start,to = end)  %>% 
+    mutate(month = month(date),return = c(NA,diff(adjusted))/lag(adjusted)) %>%
+    filter(!is.na(return))
+  months_to_use <- switching$month[which(switching$ticker == ticker)]
+  price_data <- price_data %>%
+    filter(month %in% months_to_use)
+  
+  monthly_return <- price_data %>% 
+    group_by(month) %>% 
+    summarize(return = sum(return)) %>%
+    filter(!is.na(return)) %>%
+    mutate(month = as.numeric(month)) %>%
+    arrange(month) %>%
+    mutate(ticker = rep(ticker),
+           strategy = "Sector switching strategy")
+}
+
+tidy_return <- map_dfr(.x=unique(switching$ticker),.f=return_finder)
+tidy_return <- bind_rows(tidy_return,RSP_monthly_return)
+total_return <- tidy_return %>% group_by(strategy) %>% summarize(sum(return))
