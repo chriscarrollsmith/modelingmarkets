de491cae8be0bc58b2473c98f04cfad08f861d7b
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2022-06-21 12:26:42 -0600

    Update em-us-bond-spread.R

--- em-us-bond-spread.R
+++ em-us-bond-spread.R
@@ -1,8 +1,12 @@
+#Load libraries
 library(tidyquant)
 library(tidyverse)
 library(lubridate)
 
+#Get historical price data for VWOB and GOVT from Yahoo! Finance
 df <- tq_get(c("GOVT","VWOB"))
+
+#Get and clean today's price data for VWOB and GOVT from Yahoo! Finance
 today <- getQuote(c("GOVT","VWOB"))
 today <- today %>%
   mutate(date = Sys.Date(),
@@ -10,7 +14,11 @@ today <- today %>%
 today$symbol <- rownames(today)
 today <- today %>%
   select(symbol,date,close)
+
+#Combine historical data with today's data
 df <- bind_rows(df,today)
+
+#Reorganize data frame so GOVT and VWOB are side-by-side
 df_1 <- df %>%
   filter(symbol=="GOVT") %>%
   select(date,govt_close = close)
@@ -18,13 +26,19 @@ df_2 <- df %>%
   filter(symbol=="VWOB") %>%
   select(date,vwob_close = close)
 df <- inner_join(df_1,df_2,by="date")
+
+#Calculate spread between them
 df <- df %>% 
   mutate(spread = vwob_close/govt_close)
+
+#Plot the spread
 df %>% ggplot(aes(date,spread)) +
   geom_line() +
   ggtitle("VWOB (EM bonds) / GOVT (US bonds) price ratio") +
   labs(x="Date",y="Price ratio",caption="Data courtesy of Yahoo! Finance API. Copyright Wall Street Petting Zoo, 2022.\n
         Non-adjusted price data through 2/24/2022. Average duration of both ETFs is ~8.3 years. ")
+
+#Save the plot
 ggsave("vwob-govt.jpg",
        plot = last_plot(),
        scale = 1,
