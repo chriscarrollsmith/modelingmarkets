a38e51e431a0667935d3c49f40b024bf308c0732
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2022-06-22 09:30:42 -0600

    Update C19SectorReturns.R
    Streamlining code, improving data visualization

--- C19SectorReturns.R
+++ C19SectorReturns.R
@@ -23,7 +23,7 @@ tickers <- as.data.frame(url %>%
 
 #create a vector of tickers
 sp500tickers <- tickers[["Symbol"]]
-sp500tickers = case_when(
+sp500tickers <- case_when(
   #replace dots with dashes in select ticker symbols
   sp500tickers == "BRK.B" ~ "BRK-B",
   sp500tickers == "BF.B" ~ "BF-B",
@@ -31,48 +31,52 @@ sp500tickers = case_when(
   TRUE ~ as.character(sp500tickers)
   )
 
-#Define function that returns a data frame of 
-#daily closing prices for a ticker
-get_prices <- function(ticker){
-  #tidyquant function tq_get pulls price data 
-  #for a ticker from Yahoo Finance
-  df = tq_get(ticker, from = start_date, to = Sys.Date()) %>% 
-  #Add a column with ticker symbol repeated for 
-  #number of items in data frame's "close" column
-  mutate(symbol = rep(ticker, length(close)))
-}
-
-#Call function to create price data frame for each ticker, 
-#and then bind all data frames together into one
-tickers_df = map(sp500tickers, get_prices) %>%
-  bind_rows()
+#Get historical price data for all tickers
+df <- tq_get(tickers$Symbol, from = start_date, to = Sys.Date())
 
 #Combine info from Wikipedia with our price data
 #by joining the two tables using symbol as reference point
 #select desired columns and convert symbol to factor
-tickers_df = tickers_df %>%
+df <- df %>%
   left_join(tickers, by = c('symbol' = 'Symbol')) %>%
   clean_names() %>%
   select(symbol,security,date,open,high,low,close,gics_sector,gics_sub_industry) %>%
   mutate(symbol = as.factor(symbol)) %>%
   mutate(gics_sector = as.factor(gics_sector))
 
-#adds a column to show percent change since period start
-tickers_df <- tickers_df %>% 
+#add a column to show percent change since period start
+df <- df %>% 
   group_by(symbol) %>%
   mutate(pctchange = close/close[1])
 
-#constructs vector of equal weight average percent change by sector by date
-sector_df <- tickers_df %>%
+#construct vector of equal weight average percent change by sector by date
+sector_df <- df %>%
   group_by(gics_sector,date) %>%
   summarize(pctchange = mean(pctchange))
 
-#plots sector pctchange since period start
+#Summarize total sector returns in a table
 sector_df %>%
+  summarize(return = last(pctchange)-1) %>%
+  arrange(desc(return)) %>%
+  mutate(return = scales::percent(return)) %>%
+  knitr::kable()
+
+#Pull vector of sectors in order of final returns for use in reordering factor
+refactorer <- sector_df %>%
+  summarize(return = last(pctchange)-1) %>%
+  arrange(desc(return)) %>%
+  mutate(return = scales::percent(return)) %>%
+  pull(gics_sector) %>%
+  as.character()
+
+#plot sector pct change since period start
+sector_df %>%
+  mutate(pctchange = pctchange - 1) %>%
+  mutate(gics_sector = factor(gics_sector,levels=refactorer)) %>%
   ggplot(aes(x = date,y = pctchange,color=gics_sector)) +
-  geom_line() + 
+  geom_line(lwd=1) + 
   scale_y_continuous(labels = scales::percent) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+  scale_x_date(date_breaks = "3 months", date_labels = "%b%y") +
   labs(title="S&P 500 sector return (equal weight) during the Covid-19 pandemic",x="Date",y="Percent change",color="Sector") +
   theme_bw()
 
@@ -80,7 +84,6 @@ sector_df %>%
 ggsave(
   filename = "C19SectorReturns.jpg",
   plot = last_plot(),
-  path = "", #insert path to which to save the file
   scale = 1,
   width = 1920/144,
   height = 1080/144,
